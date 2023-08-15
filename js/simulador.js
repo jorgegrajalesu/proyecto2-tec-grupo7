94c1f64dd68bfd26b4a0f5ef90058a6375f9ebbf
jorgegrajalesu <jegosystem@gmail.com>
2023-08-14 19:08:32 -0500

    140823

---
+++ js/simulador.js
@@ -0,0 +1,24 @@
+/*   crear la función de calcular 
+que está en el boton de calcular, que esta en el evento 
+onClick, la función es de tipo tradicional 
+*/
+
+// sintaxis de la función tradicional
+
+function calcular() {
+    // definir las variables como el precio y cantidad
+
+    // se va a tomar el dato del elemento input precio del html 
+    let precio = document.simuladorFactura.precio.value;
+     // se va a tomar el dato del elemento input cantidad del html
+    let cantidad = document.simuladorFactura.cantidad.value;
+
+    // definir el valor del IVA 
+    const iva = 0.19;
+
+    // capturar el dato del producto
+    producto = document.getElementById('producto').value;
+
+    
+
+}
